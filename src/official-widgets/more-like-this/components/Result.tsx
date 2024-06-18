2a18173b9cc29fcf45a08bbc710809ca1dfee2e3
Niflnir <shaun.cheng@visenze.com>
2024-06-18 11:30:51 +0800

    feat: remove aspect ratio from product card image for all rec widgets

--- src/official-widgets/more-like-this/components/Result.tsx
+++ src/official-widgets/more-like-this/components/Result.tsx
@@ -85,7 +85,7 @@ const Result: FC<ResultProps> = ({ index, result }) => {
 
   return (
     <a className={`${debugMode ? '' : 'cursor-pointer'}`} ref={targetRef} onClick={debugMode ? undefined : onClick}>
-      <Skeleton classNames={{ content: 'aspect-[2/3]' }} isLoaded={!isLoading}>
+      <Skeleton isLoaded={!isLoading}>
         <img className='object-fit size-full' src={result.im_url}/>
       </Skeleton>
       <Skeleton className='pt-2' isLoaded={!isLoading}>
