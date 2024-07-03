15361869a697f3b146c6e11691e2768d99971e4e
Wilson Kurniawan <wilson@visenze.com>
2024-07-03 16:44:19 +0800

    Remove camera button selector

--- src/official-widgets/shopping-assistant/index-dev.tsx
+++ src/official-widgets/shopping-assistant/index-dev.tsx
@@ -6,25 +6,16 @@ import { devConfigs, devFieldMappings } from './dev-configs';
 import App from './app';
 import defaultConfigs from '../../default-configs';
 
-const createElement = (id: string): HTMLDivElement => {
-  const element = document.createElement('div');
-  element.style.display = 'none';
-  element.id = id;
-  document.body.appendChild(element);
-  return element;
-};
-
-const getRenderElement = (configs: WidgetConfig): HTMLDivElement => {
-  const { cssSelector, cameraButtonSelector } = configs.displaySettings;
-  return (
-      document.body.querySelector(cssSelector || `.ps-widget-${configs.appSettings.placementId}`)
-      || document.body.querySelector(`#${cameraButtonSelector}`)
-      || createElement(cameraButtonSelector)
-  );
+const getRenderElement = (configs: WidgetConfig): HTMLElement | null => {
+  const { cssSelector } = configs.displaySettings;
+  return document.body.querySelector(cssSelector || `.ps-widget-${configs.appSettings.placementId}`);
 };
 
 const render = (client: WidgetClient, fieldMappings: Record<string, string>, configs: WidgetConfig): void => {
   const element = getRenderElement(configs);
+  if (!element) {
+    throw new Error('Element not found');
+  }
   const root = createRoot(element);
   root.render(<App productSearch={client} fieldMappings={fieldMappings} config={configs}></App>);
   client.setRenderRoots([root]);
