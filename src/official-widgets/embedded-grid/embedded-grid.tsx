3d9004b8ce815b8296e71dc03cc7c3d225e1241c
Niflnir <shaun.cheng@visenze.com>
2024-07-08 15:27:54 +0800

    feat: add data-pw for embedded grid widget

--- src/official-widgets/embedded-grid/embedded-grid.tsx
+++ src/official-widgets/embedded-grid/embedded-grid.tsx
@@ -57,14 +57,14 @@ const EmbeddedGrid: FC<EmbeddedGridProps> = ({ config, productSearch, productId 
   return (
     <>
       <WidgetResultContext.Provider value={{ metadata, productResults }}>
-        <div className='bg-primary'>
+        <div className='bg-primary' data-pw='eg-container'>
           {/* Widget Title */}
-          <div className='widget-title py-2 text-center text-primary md:py-4'>You may also like</div>
+          <div className='widget-title py-2 text-center text-primary md:py-4' data-pw='eg-widget-title'>You may also like</div>
 
-          {/* Product card grid */}
-          <div className='grid grid-cols-2 gap-x-2 gap-y-4 md:grid-cols-5'>
+          {/* Product result grid */}
+          <div className='grid grid-cols-2 gap-x-2 gap-y-4 md:grid-cols-5' data-pw='eg-product-result-grid'>
             {productResults.map((result, index) => (
-              <div key={`${result.product_id}-${index}`}>
+              <div key={`${result.product_id}-${index}`} data-pw={`eg-product-result-card-${index + 1}`}>
                 <Result
                   index={index}
                   result={result}
