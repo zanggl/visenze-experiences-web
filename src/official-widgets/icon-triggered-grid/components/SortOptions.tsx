b16b0c97f98336ab9c34ae576efeebfa5de7fdc8
Niflnir <shaun.cheng@visenze.com>
2024-07-15 14:02:08 +0800

    feat: use NextUI RadioGroup and Checkbox components for icon-triggered grid widget

--- src/official-widgets/icon-triggered-grid/components/SortOptions.tsx
+++ src/official-widgets/icon-triggered-grid/components/SortOptions.tsx
@@ -2,6 +2,7 @@ import type { FC, ReactElement } from 'react';
 import { memo, useState } from 'react';
 import { Button } from '@nextui-org/button';
 import { cn } from '@nextui-org/theme';
+import { Radio, RadioGroup } from '@nextui-org/radio';
 import { SortType } from '../../../common/types/constants';
 import type { ScreenType } from '../icon-triggered-grid';
 
@@ -23,26 +24,15 @@ const SortOptions: FC<SortOptionsProps> = ({ sortType, setSortType, setScreen, c
     <div className={cn('bg-primary', className)}>
       <div className='primary-text py-3 text-center text-lg lg:py-0 lg:text-start'>Sort</div>
       <div className='calls-to-action-text flex h-full flex-col gap-2 border-1 p-4'>
-        {/* Relevance option */}
-        <label className='flex cursor-pointer gap-2' data-pw='itg-sort-relevance'>
-          <input type='radio' id='relevance' value={SortType.RELEVANCE} checked={selectedOption === SortType.RELEVANCE}
-                 onChange={(event) => setSelectedOption(event.target.value as SortType)}/>
-          {SortType.RELEVANCE}
-        </label>
-
-        {/* Price high to low option */}
-        <label className='flex cursor-pointer gap-2' data-pw='itg-sort-high-to-low'>
-          <input type='radio' value={SortType.PRICE_HTL} checked={selectedOption === SortType.PRICE_HTL}
-                 onChange={(event) => setSelectedOption(event.target.value as SortType)}/>
-          {SortType.PRICE_HTL}
-        </label>
-
-        {/* Price low to high option */}
-        <label className='flex cursor-pointer gap-2' data-pw='itg-sort-low-to-high'>
-          <input type='radio' value={SortType.PRICE_LTH} checked={selectedOption === SortType.PRICE_LTH}
-                 onChange={(event) => setSelectedOption(event.target.value as SortType)}/>
-          {SortType.PRICE_LTH}
-        </label>
+        <RadioGroup
+          value={selectedOption}
+          onChange={(event) => setSelectedOption(event.target.value as SortType)}
+          color='secondary'
+        >
+          <Radio value={SortType.RELEVANCE} data-pw='itg-sort-relevance'>{SortType.RELEVANCE}</Radio>
+          <Radio value={SortType.PRICE_HTL} data-pw='itg-sort-high-to-low'>{SortType.PRICE_HTL}</Radio>
+          <Radio value={SortType.PRICE_LTH} data-pw='itg-sort-low-to-high'>{SortType.PRICE_LTH}</Radio>
+        </RadioGroup>
       </div>
 
       {/* Cancel & Apply buttons */}
