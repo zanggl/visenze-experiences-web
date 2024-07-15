ce7d68bf24989511cb16c876a45a546c61a56ada
Niflnir <shaun.cheng@visenze.com>
2024-07-15 11:34:21 +0800

    chore: add NextUI Spinner component

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,7 +60,7 @@ function remToPx(input, fontSize = 16) {
 module.exports = {
   content: [
     './src/**/*.{html,js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/components/(button|card|chip|image|input|listbox|spacer|skeleton).js',
+    './node_modules/@nextui-org/theme/dist/components/(button|card|chip|image|input|listbox|spacer|skeleton|spinner).js',
   ],
   theme: {
     ...remToPx(defaultTheme),
