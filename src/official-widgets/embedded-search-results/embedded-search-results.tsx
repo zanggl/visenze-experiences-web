4d1005a22ac2b435633879279edf234e880ff94a
Niflnir <shaun.cheng@visenze.com>
2024-07-24 11:38:39 +0800

    fix: unncessary api call on render

--- src/official-widgets/embedded-search-results/embedded-search-results.tsx
+++ src/official-widgets/embedded-search-results/embedded-search-results.tsx
@@ -86,7 +86,9 @@ const EmbeddedSearchResults = (): ReactElement => {
   };
 
   useEffect(() => {
-    multisearchWithSearchBarDetails();
+    if (!isLoading) {
+      multisearchWithSearchBarDetails();
+    }
   }, [selectedFilters]);
 
   useEffect(() => {
